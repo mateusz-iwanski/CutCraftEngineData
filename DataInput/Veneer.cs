de0ac0563d531907e48939e5e74e43e49bef3746
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-14 19:02:04 +0200

    Add Veneer class to represent veneer characteristics
    Introduce a new class `Veneer` in the `CutCraftEngineDataInput.DataInput` namespace. This class inherits from `DataGroupInput` and implements the `IVeneer` interface. It includes properties for `id`, `title`, `width`, `thickness`, and `maxMaterialThickness`, all initialized through the constructor. Added necessary `using` directives.

---
+++ DataInput/Veneer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineDataInput.DataInput
+{
+    /// <summary>
+    /// List of veneer objects.
+    /// </summary>
+    /// <summary>
+    /// The Veneer object specifies characteristics of a veneer.
+    /// </summary>
+    public class Veneer : DataGroupInput, IVeneer
+    {
+        /// <summary>
+        /// Unique id of the veneer.
+        /// Mandatory
+        /// </summary>
+        public int id { get; }
+
+        /// <summary>
+        /// Title of the veneer.
+        /// Mandatory
+        /// </summary>
+        public string title { get; }
+
+        /// <summary>
+        /// width of the veneer.
+        /// Mandatory
+        /// </summary>
+        public int width { get; }
+
+        /// <summary>
+        /// Thickness of the veneer.
+        /// Mandatory
+        /// </summary>
+        public double thickness { get; }
+
+        /// <summary>
+        /// The maximum thickness of the material for which this veneer can be used.
+        /// Mandatory
+        /// </summary>
+        public double maxMaterialThickness { get; }
+
+        /// <summary>
+        /// Creates a new Veneer object.
+        /// </summary>
+        /// <param name="id">Unique id of the veneer. Mandatory.</param>
+        /// <param name="title">Title of the veneer. Mandatory.</param>
+        /// <param name="width">width of the veneer. Mandatory.</param>
+        /// <param name="thickness">Thickness of the veneer. Mandatory.</param>
+        /// <param name="maxMaterialThickness">The maximum thickness of the material for which this veneer can be used. Mandatory.</param>
+        public Veneer(int id, string title, int width, double thickness, double maxMaterialThickness)
+        {
+            this.id = id;
+            this.title = title;
+            this.width = width;
+            this.thickness = thickness;
+            this.maxMaterialThickness = maxMaterialThickness;
+        }
+    }
+}
