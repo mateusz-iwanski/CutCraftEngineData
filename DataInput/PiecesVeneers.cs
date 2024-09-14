5972326f2bb2bb559d93db6f8a1a83d69aadf3b9
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-14 19:03:26 +0200

    Add Veneers class to specify edge veneers in 2D objects
    Introduced a new namespace `CutCraftEngineDataInput.DataInput` and added a `Veneers` class within it. The `Veneers` class specifies which veneers should be used on the left, right, top, and bottom edges of a 2D object. It includes four nullable integer properties: `leftVeneerId`, `rightVeneerId`, `topVeneerId`, and `bottomVeneerId`. A constructor was also added to initialize these properties.

---
+++ DataInput/PiecesVeneers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineDataInput.DataInput
+{
+    /// <summary>
+    /// Specifies which veneers should be used.
+    /// 2D Only
+    /// </summary>
+    public class Veneers : DataGroupInput
+    {
+        /// <summary>
+        /// ID of the veneer to be used on the left edge.
+        /// </summary>
+        public int? leftVeneerId { get; }
+
+        /// <summary>
+        /// ID of the veneer to be used on the right edge.
+        /// </summary>
+        public int? rightVeneerId { get; }
+
+        /// <summary>
+        /// ID of the veneer to be used on the top edge.
+        /// </summary>
+        public int? topVeneerId { get; }
+
+        /// <summary>
+        /// ID of the veneer to be used on the bottom edge.
+        /// </summary>
+        public int? bottomVeneerId { get; }
+
+        /// <summary>
+        /// Constructor for Veneers class. 2D Only
+        /// </summary>
+        /// <param name="leftVeneerId">ID of the veneer to be used on the left edge.</param>
+        /// <param name="rightVeneerId">ID of the veneer to be used on the right edge.</param>
+        /// <param name="topVeneerId">ID of the veneer to be used on the top edge.</param>
+        /// <param name="bottomVeneerId">ID of the veneer to be used on the bottom edge.</param>
+        public Veneers(int? leftVeneerId, int? rightVeneerId, int? topVeneerId, int? bottomVeneerId)
+        {
+            this.leftVeneerId = leftVeneerId;
+            this.rightVeneerId = rightVeneerId;
+            this.topVeneerId = topVeneerId;
+            this.bottomVeneerId = bottomVeneerId;
+        }
+    }
+}
