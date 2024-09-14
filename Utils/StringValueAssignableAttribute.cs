c580bf9865f8baff446a06d5ad03e3c3c053b633
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-14 19:02:59 +0200

    Add StringValueAssignableAttribute for property validation
    Created StringValueAssignableAttribute.cs with a new custom attribute class in the CutCraftEngineDataInput.Utils namespace. This attribute checks if a public string property's value is within a predefined list. It includes a constructor for initializing the list and a method to retrieve it. The attribute is restricted to properties using [System.AttributeUsage(System.AttributeTargets.Property)].

---
+++ Utils/StringValueAssignableAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CutCraftEngineDataInput.Utils
+{
+    /// <summary>
+    /// Custom attribute to check string property (only public) values against a predefined list of values.  
+    /// Using:
+    ///     public class MyClass {
+    ///         // set metadata for property
+    ///         [StringValueChecker("x", "y", ...)] - MyProperty must be one of these values
+    ///         public string MyProperty { get; set; } - can be only getter
+    ///     
+    ///     public MyClass{
+    ///         MyProperty = "z";
+    ///         // check if property is valid
+    ///         StringValueCheckAttribute<MyClass>(this);
+    ///     }
+    ///     
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Property)        
+    ]
+    public class StringValueAssignableAttribute : Attribute
+    {
+        private readonly string[] _values;
+
+        /// <summary>
+        /// Constructor for StringValueAssignableAttribute.
+        /// </summary>
+        /// <param name="values">The predefined list of string values to check against.</param>
+        public StringValueAssignableAttribute(params string[] values)
+        {
+            this._values = values;
+        }
+
+        /// <summary>
+        /// Returns the predefined list of string values.
+        /// </summary>
+        /// <returns>The predefined list of string values.</returns>
+        public string[] Values() => this._values;
+    }
+}
