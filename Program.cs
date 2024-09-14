2fb16f803f42f6ce5d8d42767f0b4d9706e63234
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-14 18:06:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CutCraftEngineDataInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
