2e9ab357956fb533192bfde5fa118fca6444893b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-14 19:03:11 +0200

    Add PieceTemplate class with PieceFurnitureMelamineBoard method
    - Added `using` directives for `System`, `System.Collections.Generic`,
      `System.Linq`, `System.Text`, and `System.Threading.Tasks`.
    - Introduced a new namespace `CutCraftEngineDataInput.DataInput`.
    - Created an internal class `PieceTemplate` within the new namespace.
    - Added a static method `PieceFurnitureMelamineBoard` to the
      `PieceTemplate` class to construct and return a `Piece` object with
      specified parameters.

---
+++ DataInput/PieceTemplate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineDataInput.DataInput
+{
+    internal class PieceTemplate
+    {
+        public static Piece PieceFurnitureMelamineBoard(
+            int id, 
+            int materialId,
+            string identifier, string description,
+            int length, int width, int quantity, string structure, 
+            Veneers veneers)
+        {
+            return new Piece(
+                id: id,
+                materialId: materialId,
+                identifier: identifier, description: description,
+                length: length, width: width,
+                shapeType: "none",
+                quantity: quantity,
+                structure: structure,
+                priority: "normal",
+                surplus: 0,
+                margin: 0,
+                veneers: veneers
+                );
+        }
+
+    }
+}
