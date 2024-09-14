bf66341708575ef291a912da6d3ae357c45883a1
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-14 19:02:15 +0200

    Add Edging class for edge configuration in DataInput
    Introduced a new class `Edging` in the `CutCraftEngineDataInput.DataInput` namespace. This class manages the configuration of edges to be cut off from a `StockItem`. It includes four public double fields: `left`, `right`, `top`, and `bottom`, representing the sizes of the edges to be cut off on the respective sides. A constructor is provided to initialize these fields.

---
+++ DataInput/StockItemEdging.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineDataInput.DataInput
+{
+    /// <summary>
+    /// Edging configuration.
+    /// </summary>
+    public class Edging : DataGroupInput
+    {
+        /// <summary>
+        /// The size of the edge to be cut off on the left side of the StockItem.
+        /// </summary>
+        public double left;
+
+        /// <summary>
+        /// The size of the edge to be cut off on the right side of the StockItem.
+        /// </summary>
+        public double right;
+
+        /// <summary>
+        /// The size of the edge to be cut off on the top of the StockItem. (2D only)
+        /// </summary>
+        public double top;
+
+        /// <summary>
+        /// The size of the edge to be cut off on the bottom of the StockItem. (2D only)
+        /// </summary>
+        public double bottom;
+
+        /// <summary>
+        /// Constructor for Edging class.
+        /// </summary>
+        /// <param name="left">The size of the edge to be cut off on the left side of the StockItem.</param>
+        /// <param name="right">The size of the edge to be cut off on the right side of the StockItem.</param>
+        /// <param name="top">The size of the edge to be cut off on the top of the StockItem.</param>
+        /// <param name="bottom">The size of the edge to be cut off on the bottom of the StockItem.</param>
+        public Edging(double left, double right, double top, double bottom)
+        {
+            this.left = left;
+            this.right = right;
+            this.top = top;
+            this.bottom = bottom;
+        }
+    }
+}
