d91af10857bd66922f7a4d461781164615f066b5
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-15 20:21:28 +0200

    Add IStockItem interface in CutCraftEngineDataInput namespace
    Introduce a new namespace `CutCraftEngineDataInput.DataInput` and
    define the `IStockItem` interface within it. The interface extends
    `IDataGroupRoot` and includes properties such as `description`,
    `edging`, `id`, `identifier`, `length`, `materialId`, `priority`,
    `quantity`, `structure`, and `width`. Additionally, it declares a
    method `dynamic SizeReal()`.

---
+++ DataInput/IStockItem.cs
@@ -0,0 +1,18 @@
+﻿namespace CutCraftEngineDataInput.DataInput
+{
+    public interface IStockItem : IDataGroupRoot
+    {
+        string description { get; }
+        Edging edging { get; }
+        int id { get; }
+        string identifier { get; }
+        double length { get; }
+        int materialId { get; }
+        string priority { get; }
+        int quantity { get; }
+        string structure { get; }
+        double width { get; }
+
+        dynamic SizeReal();
+    }
+}
