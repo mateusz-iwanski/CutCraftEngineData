67121e893efdda9b94d28e146462f631b511e11d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-15 20:19:54 +0200

    Add MaterialTemplate class with MaterialFurnitureMelamineBoard
    Introduce MaterialTemplate class in CutCraftEngineDataInput.DataInput
    namespace. This class includes a static method, MaterialFurnitureMelamineBoard,
    which constructs and returns a Material object. The method initializes the
    Material object with various properties such as id, deviceId, title, thickness,
    kerf, rauseWaste, and standardStockItems. Added necessary using directives for
    System, System.Collections.Generic, System.Linq, System.Text, and
    System.Threading.Tasks.

---
+++ DataInput/MaterialTemplate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineDataInput.DataInput
+{
+    internal class MaterialTemplate
+    {        
+        public static Material MaterialFurnitureMelamineBoard(int id, int deviceId, string title, int thickness, double kerf,
+            ReuseWaste rauseWaste, List<StandardStockItem> standardStockItems)
+        { 
+            return new Material(
+                id: id,
+                deviceId: deviceId,
+                title: title,
+                kind: "2d",
+                width: 0,
+                thickness: thickness,
+                canHaveStructure: true,
+                canRotate: false,
+                canMirror: false,
+                surplus: 0,
+                surplusEditable: true,  // surplus set in _piece 
+                margin: 0,
+                marginEditable: true,  // Margin set in _piece
+                defaultEdging: 0,
+                cuttingDimensions: "net",
+                canBeVeneered:false,
+                kerf: kerf,
+                allowEdgeCuts: true,
+                rauseWaste: rauseWaste,
+                standardStockItems: standardStockItems
+                );
+        }
+    }
+}
