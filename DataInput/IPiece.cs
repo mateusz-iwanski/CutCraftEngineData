e9cc7fea3f7374e4c63242c13fec5c23253d45a9
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-15 20:21:54 +0200

    Add IPiece interface to DataInput namespace
    Introduce a new namespace `CutCraftEngineDataInput.DataInput` and define the `IPiece` interface within it. This interface extends `IDataGroupRoot` and includes properties such as `description`, `id`, `identifier`, `length`, `margin`, `materialId`, `priority`, `quantity`, `shapeType`, `structure`, `surplus`, `veneers`, and `width`. It also declares a method `SizeReal()` returning a dynamic type.

---
+++ DataInput/IPiece.cs
@@ -0,0 +1,21 @@
+﻿namespace CutCraftEngineDataInput.DataInput
+{
+    public interface IPiece : IDataGroupRoot
+    {
+        string description { get; }
+        int id { get; }
+        string identifier { get; }
+        int length { get; }
+        int margin { get; }
+        int materialId { get; }
+        string priority { get; }
+        int quantity { get; }
+        string shapeType { get; }
+        string structure { get; }
+        int surplus { get; }
+        Veneers veneers { get; set; }
+        int width { get; }
+
+        dynamic SizeReal();
+    }
+}
