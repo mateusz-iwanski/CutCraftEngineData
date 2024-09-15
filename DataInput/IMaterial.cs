531945c1584d2235f8086f2b826aadbd6125c494
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-15 20:22:10 +0200

    Add IMaterial interface in new DataInput namespace
    Introduced a new namespace `CutCraftEngineDataInput.DataInput` and defined an `IMaterial` interface within it. This interface extends `IDataGroupRoot` and includes properties for material characteristics such as `allowEdgeCuts`, `canBeVeneered`, `canHaveStructure`, `canMirror`, `canRotate`, `cuttingDimensions`, `defaultEdging`, `deviceId`, `id`, `kerf`, `kind`, `margin`, `marginEditable`, `rauseWaste`, `standardStockItems`, `surplus`, `surplusEditable`, `thickness`, `title`, and `width`.

---
+++ DataInput/IMaterial.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CutCraftEngineDataInput.DataInput
+{
+    public interface IMaterial : IDataGroupRoot
+    {
+        bool allowEdgeCuts { get; }
+        bool canBeVeneered { get; }
+        bool canHaveStructure { get; }
+        bool canMirror { get; }
+        bool canRotate { get; }
+        string cuttingDimensions { get; }
+        int defaultEdging { get; }
+        int deviceId { get; }
+        int id { get; }
+        double kerf { get; }
+        string kind { get; }
+        int margin { get; }
+        bool marginEditable { get; }
+        ReuseWaste rauseWaste { get; }
+        List<StandardStockItem> standardStockItems { get; }
+        int surplus { get; }
+        bool surplusEditable { get; }
+        int thickness { get; }
+        string title { get; }
+        int width { get; }
+    }
+}
