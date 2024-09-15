da4ccdf380e90f989c953b84d91d7e5e37d69dce
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-15 20:22:30 +0200

    Add IDevice interface in new DataInput namespace
    Introduce a new namespace `CutCraftEngineDataInput.DataInput` and define the `IDevice` interface within it. The interface extends `IDataGroupRoot` and includes properties for device configuration such as `canCrossCuts`, `edgingCuts`, `firstCutDirection`, `fullCutsOnly`, `id`, `materialKind`, `maxCutDepth`, `maxCutLengthByLength`, `maxCutLengthByWidth`, `minCutWidth`, `originEdgingCuts`, `slants`, `stripCuts`, and `title`.

---
+++ DataInput/IDevice.cs
@@ -0,0 +1,20 @@
+﻿namespace CutCraftEngineDataInput.DataInput
+{
+    public interface IDevice : IDataGroupRoot
+    {
+        bool canCrossCuts { get; }
+        string edgingCuts { get; }
+        string firstCutDirection { get; }
+        bool fullCutsOnly { get; }
+        int id { get; }
+        string materialKind { get; }
+        MaxCutDepth maxCutDepth { get; set; }
+        MaxCutLengthByLength maxCutLengthByLength { get; }
+        MaxCutLengthByWidth maxCutLengthByWidth { get; }
+        int minCutWidth { get; }
+        string originEdgingCuts { get; }
+        Slants slants { get; }
+        bool stripCuts { get; }
+        string title { get; }
+    }
+}
