8504c304486de45629804c85671024963a81fe2b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-15 20:20:29 +0200

    Add MaterialWasteEdging class to handle default edging
    Introduce MaterialWasteEdging class in CutCraftEngineDataInput.DataInput namespace. This class specifies default edging for usable waste material with properties `enabled` and `_default`. It includes a constructor for initialization and inherits from DataGroupInput.

---
+++ DataInput/MaterialRauseWasteEdging.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineDataInput.DataInput
+{
+    /// <summary>
+    /// Specified default edging for the waste that was qualified as usable.
+    /// </summary>
+    public class MaterialWasteEdging : DataGroupInput
+    {
+        /// <summary>
+        /// Enables or disables setting the edging to the StockItem created from the usable waste.
+        /// </summary>
+        public bool enabled { get; }
+
+        /// <summary>
+        /// The default edging size. Note that edging will be applied to all edges of the new StockItem.
+        /// </summary>
+        // TODO: Change to different name, default is the system name, i can't use it now
+        [JsonProperty("default")]
+        public int _default { get; }
+
+        /// <summary>
+        /// Constructor for Edging class.
+        /// </summary>
+        /// <param name="enabled">Enables or disables setting the edging to the StockItem created from the usable waste.</param>
+        /// <param name="default">The default edging size.</param>
+        public MaterialWasteEdging(bool enabled, int _default)
+        {
+            this.enabled = enabled;
+            this._default = _default;
+        }
+    }
+}
