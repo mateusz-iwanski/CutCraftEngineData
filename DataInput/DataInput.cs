9675a7e207ae95fe49abe4fb1c78d26860e4d9c1
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-16 22:08:50 +0200

    Add DataInput class with compatibility checks
    Introduced a new class `DataInput` in the `CutCraftEngineData.DataInput` namespace, inheriting from `DataGroupInput`. This class includes properties such as `version`, `defaultUnits`, `devices`, `materials`, `pieces`, `stock`, and `veneers`. The constructor initializes these properties and performs compatibility checks between materials and pieces, as well as between devices and materials.

---
+++ DataInput/DataInput.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    public class DataInput : DataGroupInput
+    {
+        public int version { get; }
+        public DefaultUnitsInput defaultUnits { get; set; }
+        public List<IDevice> devices { get; set;  }
+        public List<IMaterial> materials { get; set;  }
+        public List<IPiece> pieces { get; set; }
+        public List<IStockItem> stock { get; set; }
+        public List<IVeneer> veneers { get; set; }
+
+        public DataInput(
+            int version, DefaultUnitsInput defaultUnits,
+            DataInputCollector<IMaterial> materials,
+            DataInputCollector<IDevice> devices,
+            DataInputCollector<IPiece> pieces, 
+            DataInputCollector<IStockItem> stock, 
+            DataInputCollector<IVeneer> veneers)
+        {
+            this.version = version;
+            this.defaultUnits = defaultUnits;
+            this.devices = devices.GetObjectList();
+            this.materials = materials.GetObjectList();
+            this.pieces = pieces.GetObjectList();
+            this.stock = stock.GetObjectList();
+            this.veneers = veneers.GetObjectList();
+
+            CheckMaterialCompatible(this.pieces, this.stock);
+            CheckDeviceCompatible(this.devices, this.materials);
+            //CheckPieceSizeWithMaterialSize(this.pieces, this.stock);
+        }
+    }
+
+    
+
+
+
+    
+}
