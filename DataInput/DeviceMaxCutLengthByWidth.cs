37cd991c94a787709c8fbaaf0b0cb94ab128f437
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-16 22:11:44 +0200

    Add MaxCutLengthByWidth class to manage cut length limits
    Introduce a new class `MaxCutLengthByWidth` in the `CutCraftEngineData.DataInput` namespace to manage the maximum vertical cut length for devices like guillotines. This class includes:
    - A boolean property `enabled` to toggle the cut length limit.
    - A double property `limit` to specify the maximum cut length.
    - A constructor to initialize these properties.
    
    Added necessary `using` directives and XML comments for documentation.

---
+++ DataInput/DeviceMaxCutLengthByWidth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    /// <summary>
+    /// Maximum vertical cut length.
+    /// It can also be used when cutting in strips - then it allows to limit the width of the strip.
+    /// This option is especially useful when the device is a guillotine.
+    /// </summary>
+    public class MaxCutLengthByWidth
+    {
+        /// <summary>
+        /// Enables or disables maximum vertical cut length.
+        /// </summary>
+        public bool enabled { get; }
+
+        /// <summary>
+        /// Maximum cut length.
+        /// </summary>
+        // TODO: what is max limit?
+        public double limit { get; }
+
+        /// <summary>
+        /// Constructor for MaxCutLengthByWidth class.
+        /// </summary>
+        /// <param name="enabled">Enables or disables maximum vertical cut length.</param>
+        /// <param name="limit">Maximum cut length.</param>            
+        public MaxCutLengthByWidth(bool enabled, double limit)
+        {
+            this.enabled = enabled;
+            this.limit = limit;
+        }
+    }
+}
