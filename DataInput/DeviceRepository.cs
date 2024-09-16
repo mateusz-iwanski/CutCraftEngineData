c5eaf72b0ac490f04d01811e9d58845c2a5fd296
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-16 22:11:58 +0200

    Add DeviceRepository class with Device initialization
    Introduced a new internal class `DeviceRepository` in the `CutCraftEngineData.DataInput` namespace. Added necessary `using` directives. The class includes a public property `device`, which is a list of `Device` objects. The constructor initializes the `device` list and adds a `Device` object via the `DeviceSektor450` method. The `DeviceSektor450` method sets various properties for the `Device` object.

---
+++ DataInput/DeviceRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    internal class DeviceRepository
+    {
+        public List<Device> device { get; set; }
+        
+        public DeviceRepository()
+        {
+            device = new List<Device>();
+            device.Add(DeviceSektor450());
+        }
+
+        private Device DeviceSektor450()
+        {
+            return new Device(
+                    id: 1,
+                    title: "BiesseSektor450",
+                    materialKind: "2d",
+                    slants: new Slants(
+                        supported: true,
+                        leftMeasurement: "fromMiddleToRight",
+                        rightMeasurement: "fromMiddleToLeft"
+                    ),
+                    canCrossCuts: true,
+                    fullCutsOnly: true,
+                    stripCuts: true,
+                    minCutWidth: 0,
+                    edgingCuts: "optimal",
+                    originEdgingCuts: "default",
+                    firstCutDirection: "any",
+                    maxCutDepth: new MaxCutDepth(
+                        enabled: true,
+                        limit: 5,
+                        includeEdging: false
+                        ),
+                    maxCutLengthByLength: new MaxCutLengthByLength(
+                        enabled: false,
+                        limit: 10000.0d
+                        ),
+                    maxCutLengthByWidth: new MaxCutLengthByWidth(
+                        enabled: false,
+                        limit: 10000.0d
+                        )
+                    );
+        }
+        
+    }
+}
