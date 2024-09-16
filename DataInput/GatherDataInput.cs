d9c1a7c0492ac0707d35abaf778f3112026dc488
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-16 22:12:28 +0200

    Add commented-out GatherDataInput class
    Introduced a new class `GatherDataInput` in the `CutCraftEngineData.DataInput` namespace. This class implements the `IDataInputCollectorAppend` interface and includes a method `Add` that takes an `IDataGroupRoot` object and returns it unchanged. Note that the entire class is currently commented out and not active in the codebase.

---
+++ DataInput/GatherDataInput.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CutCraftEngineData.DataInput
+//{
+//    public class GatherDataInput : IDataInputCollectorAppend
+//    {
+//        public IDataGroupRoot Add(IDataGroupRoot dataInput)
+//        {
+//            return dataInput;
+//        }
+//    }
+//}
