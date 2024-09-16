1a80dc535547c9b9e86215fb6b4389ac469c5eaf
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-16 22:09:49 +0200

    Add DataInputCollector<T> class for managing data inputs
    Introduce DataInputCollector<T> in CutCraftEngineData.DataInput.
    - Private list `DataInputObjects` to store data inputs.
    - Private field `_IDataInputCollectorAppend` for appending logic.
    - Constructor initializes list and assigns append handler.
    - `Append` method adds new data objects using append handler.
    - `GetObjectList` method returns the list of data inputs.

---
+++ DataInput/DataInputCollector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    public class DataInputCollector<T>
+    {
+        List<T> DataInputObjects;
+        IDataInputCollectorAppend _IDataInputCollectorAppend;
+
+        public DataInputCollector(IDataInputCollectorAppend iDataInputCollectorAppend)
+        {
+            this.DataInputObjects = new List<T> { };
+            this._IDataInputCollectorAppend = iDataInputCollectorAppend;
+        }
+
+        public void Append(T dataGroupObject)
+        {
+            this.DataInputObjects.Add(this._IDataInputCollectorAppend.Add<T>(dataGroupObject));
+        }
+
+        public List<T> GetObjectList() => this.DataInputObjects;
+    }
+}
