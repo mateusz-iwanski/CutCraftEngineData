d5c943e89074d96648999cb45189cc80ba282709
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-17 21:03:11 +0200

    Add DefaultUnits class for handling measurement units
    Introduced DefaultUnits class in CutCraftEngineData.configuration.
    This class inherits from DataGroupInput and manages default units
    for length, field, and angle measurements. It includes properties
    with StringValueAssignable attributes to restrict values to specific
    units: "mm", "cm", "in" for length; "sqmm", "sqcm", "sqm", "sqin",
    "sqft" for field; and "deg", "rad" for angle. Two constructors are
    provided: one for string parameters and another for initializing
    from a JSON object. Both constructors validate values using
    StringValueCheckAttribute. XML documentation comments are included.

---
+++ DefaultsUnits/DefaultUnits.cs
@@ -0,0 +1,67 @@
+﻿using CutCraftEngineData.DataInput;
+using CutCraftEngineData.Utils;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData..configuration
+{
+    public class DefaultUnits : DataGroupInput
+    {
+
+
+        /// <summary>
+        /// This units is used to output data in the default units.
+        /// The length unit in C# can be specified as "mm" for millimeters, "cm" for centimeters, and "in" for inches. 
+        /// The default length unit can be changed by setting the length property in the defaultUnits JSON object. 
+        /// The default length unit is a millimeter "mm".
+        /// </summary>
+        [StringValueAssignable("mm", "cm", "in")]
+        public string length { get; set; }
+
+        /// <summary>
+        /// The field unit in C# can be specified as "sqmm" for square millimeters, "sqcm" for square centimeters, "sqm" for square meters, "sqin" for square inches, and "sqft" for square feet. 
+        /// It can be changed by setting the field property in the defaultUnits JSON object. 
+        /// The default field unit is used only if the value does not specify the unit. 
+        /// For example, if the default unit is set to square meters "sqm" and you specify { "value": 250 }, then it will be treated as 250 square meters (250m2). 
+        /// However, you can still force the unit in the value by setting it as a string, such as { "value": "250sqmm" }, which will be treated as 250 square millimeters (250mm2), regardless of the default field unit. 
+        /// The default field unit is a square millimeter "sqmm".
+        /// </summary>
+        [StringValueAssignable("sqmm", "sqcm", "sqm", "sqin", "sqft")]
+        public string field { get; set; }
+
+        /// <summary>
+        /// The angle unit in C# can be specified as "rad" for radians and "deg" for degrees. 
+        /// The default angle unit can be changed by setting the angle property in the defaultUnits JSON object. 
+        /// The default angle unit is used only if the value does not specify the unit. 
+        /// For example, if the default unit is set to degrees "deg" and you specify { "value": 0.5 }, then it will be treated as 0.5 degrees (0.5°). 
+        /// However, you can still force the unit in the value by setting it as a string, such as { "value": "0.5rad" }, which will be treated as 0.5 radians, regardless of the default angle unit. 
+        /// The default angle unit is radian "rad".
+        /// </summary>
+        [StringValueAssignable("deg", "rad")]
+        public string angle { get; set;  }
+
+        public DefaultUnits(string _length, string _field, string _angle)
+        {
+            length = _length;
+            field = _field;
+            angle = _angle;
+
+            StringValueCheckAttribute<DefaultUnits>(this);
+        }
+
+        public DefaultUnits(JToken jToken)
+        {
+            length = jToken["length"].ToString();
+            field = jToken["field"].ToString();
+            angle = jToken["angle"].ToString();
+
+            StringValueCheckAttribute<DefaultUnits>(this);
+        }
+    }
+
+    
+}
