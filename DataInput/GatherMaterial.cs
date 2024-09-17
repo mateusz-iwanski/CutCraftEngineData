498e54c9de3d0bfa0ea92abf343a451960437f05
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-17 21:00:28 +0200

    Add GatherMaterial class implementing IDataInputCollectorAppend
    Introduce GatherMaterial class in CutCraftEngineData.DataInput.
    Add necessary using directives. Implement Add method in the class.

---
+++ DataInput/GatherMaterial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    internal class GatherMaterial : IDataInputCollectorAppend
+    {
+        public IMaterial Add<IMaterial>(IMaterial material)
+        {
+            return material;
+        }
+    }
+}
