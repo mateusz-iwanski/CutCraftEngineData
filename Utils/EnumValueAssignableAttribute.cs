55036c4221cace4ffea78d580c42a0f56142b097
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-17 21:04:12 +0200

    Add EnumValueAssignableAttribute class for property validation
    Introduce EnumValueAssignableAttribute in CutCraftEngineData.DataInput.
    Add necessary using directives for various namespaces.
    Decorate the class with AttributeUsage for property targets.
    Include a private Type field and a constructor to initialize it.
    Define a Values method to return the stored Type.

---
+++ Utils/EnumValueAssignableAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EnumValueAssignableAttribute : Attribute
+    {
+        private Type Object;
+
+        public EnumValueAssignableAttribute(Type _object)
+        {
+            this.Object = _object;
+        }
+        public Type Values() => this.Object;
+    }
+}
