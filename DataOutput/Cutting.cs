4777eff5fdd6b62f386a124528f24edc4c60cbc4
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-17 21:06:15 +0200

    Add Cutting class to handle cutting data in DataOutput
    Introduced a new Cutting class in the CutCraftEngineData.DataOutput namespace. This class represents a cutting object with properties like stockItemId, quantity, statistics, pieces, rest, and cuts. The constructor initializes these properties from a JToken and populates lists for pieces, rest, and cuts. Added necessary using directives for Newtonsoft.Json and Newtonsoft.Json.Linq.

---
+++ DataOutput/Cutting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CutCraftEngineData.DataOutput
+{
+    /// <summary>
+    /// Represents a cutting object.
+    /// </summary>
+    public class Cutting
+    {
+        public int stockItemId { get; set; }
+        public int quantity { get; set; }
+        public CuttingStatistics statistics { get; set; }
+        public List<CuttingPiece> pieces { get; set; }
+        public List<CuttingRest> rest { get; set; }
+        public List<Cut> cuts { get; set; }
+
+        // TODO: change JToken to JObject, we know what it will be
+        public Cutting(JToken jToken)
+        {
+
+            stockItemId = (int)jToken["stockItemId"];
+            quantity = (int)jToken["quantity"];
+            pieces = new List<CuttingPiece>();
+            rest = new List<CuttingRest>();
+            cuts = new List<Cut>();
+            statistics = new CuttingStatistics(jToken["statistics"]);
+
+            foreach (JToken piece in jToken["pieces"])
+                pieces.Add(new CuttingPiece(piece));
+
+            foreach (JToken _rest in jToken["rest"])
+                rest.Add(new CuttingRest(_rest));
+
+            foreach (JToken cut in jToken["cuts"])
+                cuts.Add(new Cut(cut));
+
+
+
+        }
+    }
+}
