d0dc9dcc9ac96f80624d7d96e9964d954804b8a4
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-17 21:01:33 +0200

    Add IDataInputCollectorAppend interface with Add<T> method
    Introduce a new interface `IDataInputCollectorAppend` in the
    `CutCraftEngineData.DataInput` namespace. This interface includes
    a generic method `Add<T>` that accepts and returns a parameter
    of type `T`. Added necessary `using` directives for common
    namespaces.

---
+++ DataInput/IDataInputCollectorAppend.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDataInputCollectorAppend
+    {
+        public T Add<T>(T dataGroupRoot);
+    }
+}
