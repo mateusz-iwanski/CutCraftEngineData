8dd90fe5246226e22151f2e90feb044408973142
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-17 21:04:27 +0200

    Add NewtonUtils class for JSON deserialization to JObject
    Introduced NewtonUtils class in CutCraftEngineData.DataInput namespace.
    Added DeserializeToJObject method to convert JSON string to JObject.
    Imported necessary namespaces for JSON handling and data processing.

---
+++ Utils/NewtonUtils.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    public class NewtonUtils
+    {
+        /// <summary>
+        /// Deserialize the input message to a JObject
+        /// </summary>
+        /// <param name="message">The input message to be deserialized</param>
+        /// <returns>A JObject representing the deserialized message</returns>
+        public static JObject DeserializeToJObject(string message)
+        {
+            Dictionary<string, object> deserializeObject = JsonConvert.DeserializeObject<Dictionary<string, object>>(message);
+            var incomingMessage = JsonConvert.SerializeObject(deserializeObject);
+            var jObject = JObject.Parse(incomingMessage);
+            return jObject;
+        }
+    }
+}
