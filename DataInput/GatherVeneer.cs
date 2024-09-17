b7c40349de7422a5f2e87312266afec6f5759358
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-17 21:01:11 +0200

    Add GatherVeneer class implementing IDataInputCollectorAppend
    Introduce GatherVeneer class in CutCraftEngineData.DataInput namespace.
    The class implements IDataInputCollectorAppend interface and includes
    an Add method that takes and returns a generic IVeneer parameter.
    Added necessary using directives for System, System.Collections.Generic,
    System.Linq, System.Text, and System.Threading.Tasks namespaces.

---
+++ DataInput/GatherVeneer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    public class GatherVeneer : IDataInputCollectorAppend
+    {
+
+        public IVeneer Add<IVeneer>(IVeneer veneer)
+        {
+            return veneer;
+        }
+    }
+}
