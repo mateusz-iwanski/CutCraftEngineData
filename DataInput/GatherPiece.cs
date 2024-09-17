9ad010e3de224a08f42581c4c691dc1b27bd0988
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-17 21:00:40 +0200

    Add GatherPiece class implementing IDataInputCollectorAppend
    Introduce a new class `GatherPiece` in the `CutCraftEngineData.DataInput`
    namespace. This class implements the `IDataInputCollectorAppend` interface
    and includes an `Add` method that returns the piece it receives as a
    parameter. Import necessary namespaces to support the new class.

---
+++ DataInput/GatherPiece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput 
+{
+    internal class GatherPiece : IDataInputCollectorAppend
+    {
+        public IPiece Add<IPiece>(IPiece piece)
+        {
+            return piece;
+        }
+    }
+}
