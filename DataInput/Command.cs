c85618160dfa7e7e494036643d16bc195ca35f10
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-18 20:20:17 +0200

    Add Command class with properties and using directives
    Introduced a new namespace `CutCraftEngineData.DataInput` and
    created a `Command` class inheriting from `DataGroupInput`.
    Added properties: `Id`, `Cmd`, `Configuration`, and `Input`.
    Implemented a parameterless constructor. Added necessary
    `using` directives for various namespaces.

---
+++ DataInput/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    /// <summary>
+    /// DataInput from the client to the websocket server
+    /// </summary>
+    public class Command : DataGroupInput
+    {
+        public int Id { get; init; }
+        public string Cmd { get; init; }
+        public Configuration.Configuration Configuration { get; init; }
+        public DataInput Input { get; init; } 
+
+        public Command()
+        {
+            return;
+        }
+
+    }
+
+}
