5137bd2b8b373f43489662e7b0206bc548447060
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-18 20:21:54 +0200

    Refactor NewtonUtils and add new extension methods
    - Change NewtonUtils to a static class.
    - Modify DeserializeToJObject to be an extension method for DataGroupInput.
    - Add a generic Deserialize<T> extension method to NewtonUtils.

--- Utils/NewtonUtils.cs
+++ Utils/NewtonUtils.cs
@@ -8,19 +8,21 @@ using System.Threading.Tasks;
 
 namespace CutCraftEngineData.DataInput
 {
-    public class NewtonUtils
+    public static class NewtonUtils
     {
         /// <summary>
         /// Deserialize the input message to a JObject
         /// </summary>
         /// <param name="message">The input message to be deserialized</param>
         /// <returns>A JObject representing the deserialized message</returns>
-        public static JObject DeserializeToJObject(string message)
+        public static JObject DeserializeToJObject(this DataGroupInput dataGroupInput, string message)
         {
             Dictionary<string, object> deserializeObject = JsonConvert.DeserializeObject<Dictionary<string, object>>(message);
             var incomingMessage = JsonConvert.SerializeObject(deserializeObject);
             var jObject = JObject.Parse(incomingMessage);
             return jObject;
         }
+
+        public static T Deserialize<T>(this DataGroupInput dataGroupInput, string message) => JsonConvert.DeserializeObject<T>(message);
     }
 }
