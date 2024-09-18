51034560311d9caf35de5354aae0f5eb6ccc3bde
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-18 20:26:32 +0200

    Add Statistics class with JSON properties and constructor
    Introduced a new namespace `CutCraftEngineData.DataOutput` and
    added a `Statistics` class within it. The class includes properties
    `_2d` and `_1d` with JSON property attributes for mapping.
    Implemented a constructor to initialize these properties using a
    `JToken` parameter. Added necessary using directives for
    `Newtonsoft.Json`, `Newtonsoft.Json.Linq`, and various `System`
    namespaces.

---
+++ DataOutput/Statistics.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataOutput
+{
+    public class Statistics
+    {
+        [JsonProperty("2d")]
+        public Statistics2d _2d { get; set; }
+        [JsonProperty("1d")]
+        public Statistics1d _1d { get; set; }
+
+        public Statistics(JToken jToken)
+        {
+            _2d = new Statistics2d(jToken["2d"]);
+            _1d = new Statistics1d(jToken["1d"]);
+        }
+    }
+}
