8eb5a1b1bed36051795c5cdd21b2328a8423b762
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-18 20:20:40 +0200

    Enhance StandardStockItem with new properties and docs
    Updated using directives to include CutCraftEngineData.Utils.
    Added XML documentation to StandardStockItem class.
    Removed parameterless constructor from StandardStockItem.
    Added properties with XML docs: id, title, length, width,
    structure, and edging.
    Introduced new constructor initializing all properties.

--- DataInput/MaterialStandardStockItem.cs
+++ DataInput/MaterialStandardStockItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CutCraftEngineData.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,61 @@ using System.Threading.Tasks;
 
 namespace CutCraftEngineData.DataInput
 {
+    /// <summary>
+    /// The StandardStockItem object is used to define standard dimensions and default characteristics of stock items.
+    /// </summary>
     public class StandardStockItem
     {
-        public StandardStockItem() { }
+        /// <summary>
+        /// Unique id of a StandardStockItem.
+        /// </summary>
+        public required int id { get; init; }
+
+        /// <summary>
+        /// Title of a StandardStockItem.
+        /// </summary>
+        public required string title { get; init;  }
+
+        /// <summary>
+        /// Length of a StandardStockItem.
+        /// </summary>
+        public required int length { get; init;  }
+
+        /// <summary>
+        /// Width of a StandardStockItem.
+        /// </summary>
+        public int width { get; init; }
+
+        /// <summary>
+        /// Specifies acceptable structures of the _piece. Allowed values: none, byLength, byWidth.
+        /// Allowed values
+        ///     - none
+        ///         The material has no structure. Is not good idea to use it, if set none optimalization not rotate the _piece.
+        ///         Is better to use
+        ///     - byLength
+        ///         The structure is horizontal (along length).
+        ///     - byWidth
+        ///         The structure is vertical (along width).
+        ///     - "none,byLength,byWidth" = The material has no structure or with structure but can be rotate.
+        ///       With this settings the _piece will can be rotated by optimalization.
+        ///     - any
+        /// </summary>
+        [StringValueAssignable("none", "byLength", "byWidth", "none,byLength,byWidth")]
+        public string structure { get; init; }
+
+        /// <summary>
+        /// Edging configuration.
+        /// </summary>
+        public Edging edging { get; init; }
+
+        public StandardStockItem(int id, string title, int length, int width, string structure, Edging edging)
+        {
+            this.id = id;
+            this.title = title;
+            this.length = length;
+            this.width = width;
+            this.structure = structure;
+            this.edging = edging;
+        }
     }
 }
