490ad4d3f026f104b386a2309aa3e3f8d975c1db
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-18 20:27:23 +0200

    Add Cut class to handle cut data in CuttingCut.cs
    Introduce a new Cut class within the CutCraftEngineData.DataOutput namespace.
    The class includes properties for start and end positions of a cut (startX, startY, endX, endY).
    Add two constructors: one for initializing with four double parameters and another for initializing with a JToken object.
    This class is designed to manage data related to cuts for a cutting or crafting engine.

---
+++ DataOutput/CuttingCut.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataOutput
+{
+    public class Cut
+    {
+        /// <summary>
+        /// Start X position of the cut.
+        /// </summary>
+        /// <param name="startX">Start X position of the cut.</param>
+        public double startX { get; set; }
+
+        /// <summary>
+        /// Start Y position of the cut.
+        /// </summary>
+        /// <param name="startY">Start Y position of the cut.</param>
+        public double startY { get; set; }
+
+        /// <summary>
+        /// End X position of the cut.
+        /// </summary>
+        /// <param name="endX">End X position of the cut.</param>
+        public double endX { get; set; }
+
+        /// <summary>
+        /// End Y position of the cut.
+        /// </summary>
+        /// <param name="endY">End Y position of the cut.</param>
+        public double endY { get; set; }
+
+        /// <summary>
+        /// Constructor for Cut class.
+        /// </summary>
+        /// <param name="startX">Start X position of the cut.</param>
+        /// <param name="startY">Start Y position of the cut.</param>
+        /// <param name="endX">End X position of the cut.</param>
+        /// <param name="endY">End Y position of the cut.</param>
+        public Cut(double startX, double startY, double endX, double endY)
+        {
+            this.startX = startX;
+            this.startY = startY;
+            this.endX = endX;
+            this.endY = endY;
+        }
+
+        public Cut(JToken jToken)
+        {
+            startX = (double)jToken["startX"];
+            startY = (double)jToken["startY"];
+            endX = (double)jToken["endX"];
+            endY = (double)jToken["endY"];
+
+        }
+    }
+}
