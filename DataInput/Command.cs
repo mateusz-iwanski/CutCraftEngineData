429c438f77ee1659e12a6f606416bf1b9d5b8ce6
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-20 19:30:07 +0200

    Refactor Command class and add DataInputWrapper
    Updated using directives to include additional namespaces:
    CutCraftEngineData.Configuration, Newtonsoft.Json, System.Drawing,
    and System.Runtime.Intrinsics.X86.
    
    Refactored Command class:
    - Removed properties: Id, Cmd, Configuration, and Input.
    - Added properties: id (int), Cmd (CommandName) with JSON
      attribute 'cmd', Config (Configuration.Configuration) with
      JSON attribute 'config', InputWrapper (DataInputWrapper) with
      JSON attribute 'input', and AllowOverstock (bool) with JSON
      attribute 'allowOverstock' and detailed summary.
    
    Introduced new class DataInputWrapper with a single property
    'input' of type DataInput.

--- DataInput/Command.cs
+++ DataInput/Command.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using CutCraftEngineData.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,16 +16,46 @@ namespace CutCraftEngineData.DataInput
     /// </summary>
     public class Command : DataGroupInput
     {
-        public int Id { get; init; }
-        public string Cmd { get; init; }
-        public Configuration.Configuration Configuration { get; init; }
-        public DataInput Input { get; init; } 
+        public int id { get; init; }
 
+        /// <summary>
+        /// Command name sent from the client
+        /// </summary>
+        [JsonProperty("cmd")]
+        public CommandName Cmd { get; init; }
+
+        /// <summary>
+        /// Config data group contains just a list of profiles to be used.
+        /// </summary>
+        [JsonProperty("config")]
+        public Configuration.Configuration Config { get; init; }
+
+        /// <summary>
+        /// Input data group contains input data from the client.
+        /// </summary>
+        [JsonProperty("input")]
+        public DataInputWrapper InputWrapper { get; init; }
+
+        /// <summary>
+        /// The allowOverstock parameter controls the behavior of the cutting optimization algorithm. 
+        /// When set to true, the algorithm is permitted to allocate more stock items than currently available. 
+        /// This can be useful in scenarios where future stock replenishment is anticipated or over-allocation is acceptable. 
+        /// Please note that enabling this option may lead to situations where demand exceeds supply.
+        /// Note that it applies only to standard stock sizes.So, in order to use more stock items than available, 
+        /// you have to define standard stock items in material definition.
+        /// Then, you have to add stock item with the same standard size.
+        /// </summary>
+        [JsonProperty("allowOverstock")]
+        public bool AllowOverstock { get; init; }
         public Command()
         {
             return;
         }
+    }
 
+    public class DataInputWrapper
+    {
+        public DataInput input { get; set; }
     }
 
 }
