57cf7f5e48f1b9475d2962cc0d233e0dbcbf2484
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-20 19:31:47 +0200

    Add CuttingStatistics class with JSON properties
    Introduced a new namespace `CutCraftEngineData.DataOutput` and added the `CuttingStatistics` class to represent cutting object statistics. The class includes `_1d` and `_2d` properties, mapped to JSON properties using `JsonProperty` attributes. Added a constructor to initialize `_2d` from a `JToken`. Included necessary `using` directives.

---
+++ DataOutput/CuttingStatistics.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataOutput
+{
+    /// <summary>
+    /// Represents the statistics for a cutting object.
+    /// </summary>
+    public class CuttingStatistics
+    {
+        [JsonProperty("1d")]
+        CuttingStatistics1d _1d { get; set; }
+        [JsonProperty("2d")]
+        CuttingStatistics2d _2d { get; set; }
+
+        public CuttingStatistics(JToken jToken)
+        {
+            // this._1d = new CuttingStatistics1d(jToken["1d"]);
+            _2d = new CuttingStatistics2d(jToken["2d"]);
+        }
+    }
+}
