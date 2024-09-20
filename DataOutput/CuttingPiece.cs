e21cdeb37841031ff5ed7b13de3a08c4313216bd
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-20 19:31:13 +0200

    Add CuttingPiece class to DataOutput namespace
    Introduce CuttingPiece class with properties for pieceId, x, y,
    rotated, and mirrored. Add two constructors: one with parameters
    and another using a JToken object. Note that the mirrored property
    is commented out in the JToken constructor. Include XML comments
    for documentation.

---
+++ DataOutput/CuttingPiece.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataOutput
+{
+    /// <summary>
+    /// Represents a _piece with its position and orientation.
+    /// </summary>
+    public class CuttingPiece
+    {/// <summary>
+     /// ID of the _piece.
+     /// </summary>
+        public int pieceId { get; set; }
+
+        /// <summary>
+        /// x position of the _piece.
+        /// </summary>
+        public double x { get; set; }
+
+        /// <summary>
+        /// y position of the _piece.
+        /// </summary>
+        public double y { get; set; }
+
+        /// <summary>
+        /// Specifies if the _piece was rotated.
+        /// </summary>
+        public bool rotated { get; set; }
+
+        /// <summary>
+        /// Specifies if the _piece was reversed (back to front). 
+        /// Temporarily 1D only.
+        /// </summary>
+        public bool mirrored { get; set; }
+
+        /// <summary>
+        /// Constructor for the Piece class.
+        /// </summary>
+        /// <param name="pieceId">ID of the _piece.</param>
+        /// <param name="x">x position of the _piece.</param>
+        /// <param name="y">y position of the _piece.</param>
+        /// <param name="rotated">Specifies if the _piece was rotated.</param>
+        /// <param name="mirrored">Specifies if the _piece was reversed (back to front).</param>
+        public CuttingPiece(int pieceId, double x, double y, bool rotated, bool mirrored)
+        {
+            this.pieceId = pieceId;
+            this.x = x;
+            this.y = y;
+            this.rotated = rotated;
+            this.mirrored = mirrored;
+        }
+
+        public CuttingPiece(JToken jObject)
+        {
+            pieceId = (int)jObject["pieceId"];
+            x = (double)jObject["x"];
+            y = (double)jObject["y"];
+            rotated = (bool)jObject["rotated"];
+            // mirrored = (bool)jObject["mirrored"];
+        }
+    }
+}
