a93d84a2fc01e4c5e3525f8dd0758251073149d3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-20 19:32:21 +0200

    Add DataOutputs class with JSON constructor
    Added several `using` directives for necessary namespaces.
    Introduced a new namespace `CutCraftEngineData.DataOutput`.
    Added a new class `DataOutputs` within this namespace.
    The `DataOutputs` class includes:
    - A public integer field `version`.
    - A public field `defaultUnits` of type `DefaultUnits`.
    - A public field `statistics` of type `Statistics`.
    - A public list `cuttings` of type `List<Cutting>`.
    Added a constructor for `DataOutputs` that initializes fields
    from a provided `JObject`.

---
+++ DataOutput/DataOutput.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataOutput
+{
+    /// <summary>
+    /// Output data group contains optimization results.
+    /// </summary>
+    public class DataOutputs
+    {
+        /// <summary>
+        /// Version number of the data group.
+        /// </summary>
+        public int version;
+        /// <summary>
+        /// Default units specification as described in defaultUnits section.
+        /// </summary>
+        public DefaultUnits defaultUnits;
+        /// <summary>
+        /// Global statistics of the entire optimization.
+        /// </summary>
+        public Statistics statistics;
+        /// <summary>
+        /// List of cutting objects.
+        /// </summary>
+        public List<Cutting> cuttings;
+
+        /// <summary>
+        /// Initializes a new instance of the class. JObject is a Object from Newtonsoft.Json.
+        /// In such a nested structure of object we can't use JsonConvert.DeserializeObject
+        /// </summary>
+        /// <param name="jobject">Newtonsoft.Json JObject</param>        
+        public DataOutputs(JObject jobject)
+        {
+            version = (int)jobject["version"];
+            defaultUnits = new DefaultUnits(jobject["defaultUnits"]);
+            statistics = new Statistics(jobject["statistics"]);
+            cuttings = new List<Cutting>();
+
+            foreach (JToken cutting in jobject["cuttings"])
+                cuttings.Add(new Cutting(cutting));
+        }
+    }
+}
