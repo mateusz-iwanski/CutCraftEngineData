e6ec11afbd2dc2e18a9dc341ebd796a9a984c43d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-20 19:32:34 +0200

    Add Statistics1d class for 1D cutting statistics
    Added a new namespace `CutCraftEngineData.DataOutput` and introduced the `Statistics1d` class to represent statistics for 1D cutting of materials. The class includes properties for length, used length, waste length, unused length, cut count, and cuts length. Two constructors were added: one parameterized and one for JSON deserialization using `JToken`. Necessary `using` directives were also included.

---
+++ DataOutput/Statistics1d.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataOutput
+{
+    /// <summary>
+    /// Represents the statistics for 1D cutting if the material type is 1D.
+    /// </summary>
+    public class Statistics1d
+    {
+        /// <summary>
+        /// The total length of all used stock items.
+        /// </summary>
+        public double length { get; set; }
+
+        /// <summary>
+        /// The total length of all used pieces.
+        /// </summary>
+        public double usedLength { get; set; }
+
+        /// <summary>
+        /// The total waste length.
+        /// </summary>
+        public double wasteLength { get; set; }
+
+        /// <summary>
+        /// The total length of usable waste.
+        /// </summary>
+        public double unusedLength { get; set; }
+
+        /// <summary>
+        /// The number of cuts.
+        /// </summary>
+        public int cutCount { get; set; }
+
+        /// <summary>
+        /// The total length of all the cuts.
+        /// </summary>
+        public double cutsLength { get; set; }
+
+        /// <summary>
+        /// Constructor for the OneD class.
+        /// </summary>
+        /// <param name="length">The total length of all used stock items.</param>
+        /// <param name="usedLength">The total length of all used pieces.</param>
+        /// <param name="wasteLength">The total waste length.</param>
+        /// <param name="unusedLength">The total length of usable waste.</param>
+        /// <param name="cutCount">The number of cuts.</param>
+        /// <param name="cutsLength">The total length of all the cuts.</param>
+        public Statistics1d(double length, double usedLength, double wasteLength, double unusedLength, int cutCount, double cutsLength)
+        {
+            this.length = length;
+            this.usedLength = usedLength;
+            this.wasteLength = wasteLength;
+            this.unusedLength = unusedLength;
+            this.cutCount = cutCount;
+            this.cutsLength = cutsLength;
+        }
+
+        public Statistics1d(JToken jToken)
+        {
+            length = (double)jToken["length"];
+            usedLength = (double)jToken["usedLength"];
+            wasteLength = (double)jToken["wasteLength"];
+            unusedLength = (double)jToken["unusedLength"];
+            cutCount = (int)jToken["cutCount"];
+            cutsLength = (double)jToken["cutsLength"];
+        }
+    }
+}
