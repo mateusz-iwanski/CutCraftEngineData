300f95d63add67949ac982de03cbb1e7de1888a6
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-20 19:34:46 +0200

    Add CommandName enum for job-related commands
    Introduced a new file `CommandName.cs` with necessary using directives.
    Defined a new namespace `CutCraftEngineData.DataInput`.
    Added an enumeration `CommandName` listing several job-related commands:
    - cmdAddJob
    - cmdListJobs
    - cmdRemoveJob
    - cmdGetJobInfo
    - cmdGetStatus
    This enum provides a list of available commands for the system.

---
+++ DataInput/CommandName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData.DataInput
+{
+    /// <summary>
+    /// List of available commands
+    /// </summary>
+    public enum CommandName
+    {
+        cmdAddJob,
+        cmdListJobs,
+        cmdRemoveJob,
+        cmdGetJobInfo,
+        cmdGetStatus
+    }
+}
