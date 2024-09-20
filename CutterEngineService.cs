f51a35e102b8ea65cae190aff3735f217dc28595
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-20 19:34:09 +0200

    Add CutterEngineService class and necessary namespaces
    Introduced the `CutCraftEngineData` namespace and created the internal
    `CutterEngineService` class. Added a constructor to `CutterEngineService`
    that deserializes a `Command` object from a `websocketEcho` string.
    Included several `using` directives for required namespaces.

---
+++ CutterEngineService.cs
@@ -0,0 +1,21 @@
+﻿using CutCraftEngineData.DataInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineData
+{
+    internal class CutterEngineService
+    {
+        public CutterEngineService(string websocketEcho, string serviceName) 
+        {
+            Command command = new Command().Deserialize<Command>(websocketEcho);
+            // from di get service by name with interface ICalculator
+
+
+        }
+    }
+}
