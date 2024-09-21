49e5d0375136e31ac61bb670ee664494ac3d260f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-21 13:36:56 +0200

    Add CSVPieceReader class to read CSV and create Piece objects
    Introduced a new class `CSVPieceReader` in the `CutCraftEngineData.DataInput` namespace to read CSV files and create `Piece` objects. The class includes a `FilePath` property and a constructor to initialize it. The `CreatePiece` method uses `TextFieldParser` to process the CSV data, skipping the header row, and yields `Piece` objects with parsed values. The method sets the delimiter to ";" and converts fields to appropriate data types.

---
+++ Utils/CSVPieceReader.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CutCraftEngineData.DataInput
+{
+    public class CSVPieceReader
+    {
+        public string FilePath { get; set; }
+
+        public CSVPieceReader()
+        {
+
+            FilePath = @"";
+        }
+
+        public IEnumerable<Piece> CreatePiece() 
+        {
+            using (TextFieldParser parser = new TextFieldParser(FilePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(";");
+                int count = 0;
+                while (!parser.EndOfData)
+                {
+
+                    string[] fields = parser.ReadFields();
+                    if (count > 0)
+                    {
+                        yield return new Piece(
+                            id: Convert.ToInt32(fields[0]), materialId: Convert.ToInt32(fields[1]),
+                            identifier: "", description: "", length: Convert.ToInt32(fields[2]), width: Convert.ToInt32(fields[3]),
+                            shapeType: "none", quantity: Convert.ToInt32(fields[4]), structure:
+                            fields[5], priority: "normal", surplus: 0, margin: 0,
+                            veneers: new Veneers(leftVeneerId: null, rightVeneerId: null, topVeneerId: null, bottomVeneerId: null));
+                    }
+                    count++;
+                }
+            }
+
+        }
+
+    }
+}
