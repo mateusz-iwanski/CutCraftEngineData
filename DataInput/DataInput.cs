01f0234f9bf609122e4b4a23f7817fee3267cfe3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-21 13:37:51 +0200

    Refactor DataInput class and update dependencies
    Expanded `using` directives to include additional namespaces.
    Added XML documentation to `DataInput` class and properties.
    Changed `version` property to read-write (`get; set;`).
    Replaced interfaces with concrete classes in various places.
    Added a parameterless constructor to `DataInput` for deserialization.
    Updated constructor parameters to use concrete classes.
    Commented out `CheckDeviceCompatible` method call in constructor.
    Added comments to `CheckPieceSizeWithMaterialSize` method call.

--- DataInput/DataInput.cs
+++ DataInput/DataInput.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,23 +7,59 @@ using System.Threading.Tasks;
 
 namespace CutCraftEngineData.DataInput
 {
+    /// <summary>
+    /// Input data group contains input data from the client.
+    /// </summary>
     public class DataInput : DataGroupInput
     {
-        public int version { get; }
+        /// <summary>
+        /// Version number of the data group.
+        /// </summary>
+        public int version { get; set; } 
+
+        /// <summary>
+        /// The DefaultUnits property specifies the default units for various measurements in the data group. 
+        /// </summary>
         public DefaultUnitsInput defaultUnits { get; set; }
-        public List<IDevice> devices { get; set;  }
-        public List<IMaterial> materials { get; set;  }
-        public List<IPiece> pieces { get; set; }
-        public List<IStockItem> stock { get; set; }
-        public List<IVeneer> veneers { get; set; }
+
+        /// <summary>
+        /// List of device objects.
+        /// </summary>
+        public List<Device> devices { get; set;  }
+
+        /// <summary>
+        /// List of material objects.
+        /// </summary>
+        public List<Material> materials { get; set;  }
+
+        /// <summary>
+        /// List of pieces objects.
+        /// </summary>
+        public List<Piece> pieces { get; set; }
+
+        /// <summary>
+        /// List of stocks objects.
+        /// </summary>
+        public List<StockItem> stock { get; set; }
+
+        /// <summary>
+        /// List of veneers objects.
+        /// </summary>
+        public List<Veneer> veneers { get; set; }
+
+        /// <summary>
+        /// Parameterless constructor to avoid using a logical 
+        /// constructor during deserialization
+        /// </summary>
+        public DataInput() {}
 
         public DataInput(
             int version, DefaultUnitsInput defaultUnits,
-            DataInputCollector<IMaterial> materials,
-            DataInputCollector<IDevice> devices,
-            DataInputCollector<IPiece> pieces, 
-            DataInputCollector<IStockItem> stock, 
-            DataInputCollector<IVeneer> veneers)
+            DataInputCollector<Material> materials,
+            DataInputCollector<Device> devices,
+            DataInputCollector<Piece> pieces,
+            DataInputCollector<StockItem> stock,
+            DataInputCollector<Veneer> veneers)
         {
             this.version = version;
             this.defaultUnits = defaultUnits;
@@ -33,7 +70,10 @@ namespace CutCraftEngineData.DataInput
             this.veneers = veneers.GetObjectList();
 
             CheckMaterialCompatible(this.pieces, this.stock);
-            CheckDeviceCompatible(this.devices, this.materials);
+            //CheckDeviceCompatible(this.devices, this.materials);
+
+
+
             //CheckPieceSizeWithMaterialSize(this.pieces, this.stock);
         }
     }
