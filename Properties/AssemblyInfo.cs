5e4b3c9e068ff538c1cb63d2b96263e53c552b78
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-22 22:21:35 +0200

    Add assembly attributes and using directives
    Added `using System.Reflection;`, `using System.Runtime.CompilerServices;`, and `using System.Runtime.InteropServices;` directives. Included `AssemblyTrademark` and `AssemblyCulture` attributes set to empty strings. Set `ComVisible` attribute to `false` and added a `Guid` attribute for COM visibility.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("71911557-d307-4af0-91a0-fb8d09b39fd2")]
