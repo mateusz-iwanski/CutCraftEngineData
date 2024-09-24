e02c777f8e1f9b3cac0bb9cf5aef5c69f94b0392
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-24 10:00:00 +0200

    Refactor DataOutputs class and add new fields
    - Added `using` directive for `CutCraftEngineData.DefaultUnits` in `DataOutput.cs`.
    - Changed `version` field in `DataOutputs` class from `int` to `string`.
    - Updated `defaultUnits` field to use `DefaultUnits.DefaultUnits` type.
    - Added new `statistics` field of type `Statistics`.
    - Added new `cuttings` field of type `List<Cutting>`.
    - Added parameterless constructor to `DataOutputs` class for deserialization.
    - Updated constructor to initialize `version` using `ToString()` and `defaultUnits` using `DefaultUnits.DefaultUnits`.

--- DataOutput/DataOutput.cs
+++ DataOutput/DataOutput.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CutCraftEngineData.DefaultUnits;
 
 namespace CutCraftEngineData.DataOutput
 {
@@ -16,19 +17,28 @@ namespace CutCraftEngineData.DataOutput
         /// <summary>
         /// Version number of the data group.
         /// </summary>
-        public int version;
+        public string version;
+
         /// <summary>
         /// Default units specification as described in defaultUnits section.
         /// </summary>
-        public DefaultUnits defaultUnits;
+        public DefaultUnits.DefaultUnits defaultUnits;
+
         /// <summary>
         /// Global statistics of the entire optimization.
         /// </summary>
         public Statistics statistics;
+
         /// <summary>
         /// List of cutting objects.
         /// </summary>
         public List<Cutting> cuttings;
+
+        /// <summary>
+        /// Parameterless constructor to avoid using a logical 
+        /// constructor during deserialization
+        /// </summary>
+        public DataOutputs() { }
 
         /// <summary>
         /// Initializes a new instance of the class. JObject is a Object from Newtonsoft.Json.
@@ -37,8 +47,8 @@ namespace CutCraftEngineData.DataOutput
         /// <param name="jobject">Newtonsoft.Json JObject</param>        
         public DataOutputs(JObject jobject)
         {
-            version = (int)jobject["version"];
-            defaultUnits = new DefaultUnits(jobject["defaultUnits"]);
+            version = jobject["version"].ToString();
+            defaultUnits = new DefaultUnits.DefaultUnits(jobject["defaultUnits"]);
             statistics = new Statistics(jobject["statistics"]);
             cuttings = new List<Cutting>();
 
